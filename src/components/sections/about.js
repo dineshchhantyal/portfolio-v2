70bb9e78ea0deca97cce658061121bcba3a3ebcc
Dinesh Chhantyal <myagdichhantyal@gmail.com>
2024-01-19 08:22:19 -0600

    Update alt text for avatar image in about section

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -130,7 +130,7 @@ const About = ({ data }) => {
         </StyledContent>
         <StyledPic>
           <StyledAvatarLink href={github}>
-            <StyledAvatar fluid={avatar.childImageSharp.fluid} alt="Avatar" />
+            <StyledAvatar fluid={avatar.childImageSharp.fluid} alt="Dinesh Chhantyal's Picture" />
           </StyledAvatarLink>
         </StyledPic>
       </StyledFlexContainer>
