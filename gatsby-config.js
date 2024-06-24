5dcc03ab819f06f40431bdfbf3096e5b0b54b44a
Dinesh Chhantyal <myagdichhantyal@gmail.com>
2024-06-24 07:54:39 -0500

    chore: Update gatsby-config.js to add gatsby-transformer-sharp plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,6 +11,17 @@ module.exports = {
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-transformer-sharp`,
+      options: {
+        createImages: ({ node, actions }) => {
+          const { createNodeField } = actions;
+          // Your custom logic here
+        },
+        // Other options...
+      },
+    },
+
     `gatsby-plugin-sitemap`,
     `gatsby-plugin-robots-txt`,
     {
